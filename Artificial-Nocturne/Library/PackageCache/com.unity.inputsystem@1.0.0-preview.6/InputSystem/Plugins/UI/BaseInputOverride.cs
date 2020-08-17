92000f52f1950394088e6d380f4aa20e686dc702
Martnick1 <matthewmartnick@gmail.com>
2020-08-17 14:37:06 -0400

    demo completion update

--- Artificial-Nocturne/Library/PackageCache/com.unity.inputsystem@1.0.0-preview.6/InputSystem/Plugins/UI/BaseInputOverride.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine.EventSystems;
-
-namespace UnityEngine.InputSystem.UI
-{
-    internal class BaseInputOverride : BaseInput
-    {
-        public override string compositionString { get; }
-    }
-}
